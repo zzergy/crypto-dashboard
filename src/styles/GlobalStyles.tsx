a45091db3e1723557cf0c084b077ef5ebcf6c17b
Bogdana Yaneva <bogdana.yaneva@wiara.co>
2025-09-21 20:44:22 +0300

    Update Global styling

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,28 +1,34 @@
 import { GlobalStyles as MUIGlobalStyles } from '@mui/material';
 
 export const GlobalStyles = () => (
-  <MUIGlobalStyles
-    styles={{
-      html: {
-        margin: 0,
-        padding: 0,
-        width: '100%',
-        height: '100%',
-      },
-      body: {
-        margin: 0,
-        padding: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'transparent',
-      },
-      '#root': {
-        width: '100%',
-        height: '100%',
-      },
-      '*': {
-        boxSizing: 'border-box',
-      },
-    }}
-  />
+    <MUIGlobalStyles
+        styles={{
+            html: {
+                margin: 0,
+                padding: 0,
+                width: '100%',
+                height: '100%',
+            },
+            body: {
+                margin: 0,
+                padding: 0,
+                width: '100%',
+                height: '100%',
+            },
+            '#root': {
+                width: '100%',
+                height: '100%',
+            },
+            '*': {
+                boxSizing: 'border-box',
+            },
+            '*:focus': {
+                outline: 'none',
+            },
+            '*:focus-visible': {
+                outline: '2px solid #BB33FF',
+                outlineOffset: 2,
+            },
+        }}
+    />
 );

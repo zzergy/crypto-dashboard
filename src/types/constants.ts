6c7d3d54c3502c9eea39640ce6a7cc0bbe3c3f43
Bogdana Yaneva <bogdana.yaneva@wiara.co>
2025-09-23 18:14:36 +0300

    Create a list of most popular tokens

---
+++ src/types/constants.ts
@@ -0,0 +1,22 @@
+export enum TopEthereumTokens {
+    ETH = 'ETH', // Ethereum
+    USDT = 'USDT', // Tether
+    USDC = 'USDC', // USD Coin
+    DAI = 'DAI', // Dai Stablecoin
+    WBTC = 'WBTC', // Wrapped Bitcoin
+    LINK = 'LINK', // Chainlink
+    UNI = 'UNI', // Uniswap
+    MKR = 'MKR', // Maker
+    AAVE = 'AAVE', // Aave
+    SUSHI = 'SUSHI', // SushiSwap
+    COMP = 'COMP', // Compound
+    YFI = 'YFI', // Yearn Finance
+    CRV = 'CRV', // Curve DAO Token
+    SNX = 'SNX', // Synthetix
+    BAT = 'BAT', // Basic Attention Token
+    INCH1 = '1INCH', // 1inch
+    ENJ = 'ENJ', // Enjin Coin
+    GRT = 'GRT', // The Graph
+    CEL = 'CEL', // Celsius
+    LRC = 'LRC', // Loopring
+}

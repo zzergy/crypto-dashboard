ade5c695db734db2d5bab9c2074d6477d577c795
Bogdana Yaneva <bogdana.yaneva@wiara.co>
2025-09-28 22:09:52 +0300

    Format data for chart util

--- src/utils/formatDataForChart.ts
+++ src/utils/formatDataForChart.ts
@@ -1,9 +1,5 @@
 import { HistoricalPriceDataPoint } from 'alchemy-sdk';
 
-/**
- * Formats the data for chart representation.
- * @param data - The array of data points to format.
- */
 export const formatDataForChart = (data: HistoricalPriceDataPoint[]) => {
     if (!data || data.length === 0) return [];
 
